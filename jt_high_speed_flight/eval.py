ee5afaea388f52a27998fb5dc2b1bf035e783b25
qinglai <2712043819@qq.com>
2026-07-26 19:47:29 +0800

    增加注释并修改错误

--- jt_high_speed_flight/eval.py
+++ jt_high_speed_flight/eval.py
@@ -13,8 +13,8 @@ from airsim.types import AngleLevelControllerGains, PIDGains, AngleRateControlle
 import numpy as np
 from tqdm import tqdm
 
-class VideoRecorder:
-    def __init__(self, output, w, h, fps=15, pix_fmt='rgb24') -> None:
+class VideoRecorder: #深度图录制
+    def __init__(self, output, w, h, fps=15, pix_fmt='rgb24') -> None: #rgb24每个通道8bit，y8单通道
         self.p = None
         self.output = output
         command = [
@@ -25,16 +25,17 @@ class VideoRecorder:
             '-s', f'{w}x{h}',  # size of one frame
             '-pix_fmt', pix_fmt,
             '-r', f'{fps}',  # frames per second
-            '-i', '-',  # The imput comes from a pipe
+            '-i', '-',  # The input comes from a pipe
             # '-qp', '0',
             '-s', f'{w//2*2}x{h//2*2}',
             '-an',  # Tells FFMPEG not to expect any audio
             # '-c:v', 'h264_nvenc',
             # '-preset', 'fast',
-            '-loglevel', 'error',
-            '-pix_fmt', 'yuv420p'
+            '-loglevel', 'error', #只输出错误信息
+            '-pix_fmt', 'yuv420p' #输出格式
         ]
         self.p = subprocess.Popen(command + [self.output], stdin=subprocess.PIPE)
+        #启动子进程ffmpeg，并创建pipe管道
 
     def add_image(self, image):
         self.p.stdin.write(image)
@@ -52,10 +53,11 @@ class Rate:
     def sleep(self):
         while True:
             to_sleep = 1 / self.hz - time() + self.t0
-            if to_sleep < 0.01:
+            if to_sleep < 0.01: 
                 break
             sleep(to_sleep)
-        self.t0 += max(1 / self.hz, 0.5 / self.hz - to_sleep)
+        self.t0 += max(1 / self.hz, 0.5 / self.hz - to_sleep) 
+        #正常或略微晚一点时t0=t0+T，否则变成t0=当前时间-T/2（卡顿后不完整等待一个周期）
 
 
 import torch
@@ -107,9 +109,9 @@ args = parser.parse_args()
 print(args)
 
 
-# hover_thr = 0.297
-hover_thr = 0.5
-datetime_str = datetime.now().strftime("%Y%m%d_%H%M%S")
+hover_thr = 0.297 #airsim中油门线性，这里可能有改过无人机模型
+# hover_thr = 0.593
+datetime_str = datetime.now().strftime("%Y%m%d_%H%M%S") #年月日_时分秒
 log_dir = f'exps_{args.target_speed}/{datetime_str}/'
 os.makedirs(log_dir)
 
@@ -123,26 +125,28 @@ agents = {
         ("drone_6", [[ 6,  1.2, 0], [ 0, -1.2, 0]]),
     ],
     "single": [
-        ("drone_1", [[0, 0, -2], [20, 20, -2]]),
-    ],
+        ("drone_1", [[0, 1.2, -1], [6, 1.2, -1]]),
+    ]
 }[args.env]
 
 depth_recorder = VideoRecorder(f'{log_dir}/depth.mp4', 16*args.sr, len(agents)*12*args.sr, pix_fmt='y8')
 
 B = len(agents)
-agent_names, agent_waypoints = zip(*agents)
+agent_names, agent_waypoints = zip(*agents) #先把外层列表拆开，zip按收到的参数进行整合返回
 target_pos = [w[-1] for w in agent_waypoints]
 traj_history = {agent_name: [] for agent_name in agent_names}
 
 # connect to the AirSim simulator
-client = airsim.MultirotorClient()
-client.confirmConnection()
-client.reset()
+client = airsim.MultirotorClient() #客户端对象，连接本机airsim服务，端口41451
+client.confirmConnection() #发送测试请求，检查通信是否正常
+client.reset() #仿真大致恢复到启动时状态
 
 device = torch.device('cuda')
-model = Model(7 if args.no_odom else 10, 6).eval().to(device)
+model = Model(7 if args.no_odom else 10, 6).eval().to(device) 
+#切换到推理模式，停止dropout和batchnorm，切换到GPU。
+#想要切换回到训练模式，调用model.train()即可
 if args.resume:
-    model.load_state_dict(torch.load(args.resume, map_location=device))
+    model.load_state_dict(torch.load(args.resume, map_location=device)) #加载参数
 
 @torch.no_grad()
 def main():
@@ -151,35 +155,35 @@ def main():
         _, _, h = model(
             torch.zeros(B, 1, 12, 16, device=device),
             torch.zeros(B, model.v_proj.in_features, device=device),
-            h)
+            h) #并没真正用到后续隐藏状态中，仅为模型和cuda预热
 
     sleep(1)
     for agent_name, waypoints in agents:
-        sleep(0.1)
-        client.enableApiControl(True, agent_name)
-        client.armDisarm(True, agent_name)
+        sleep(0.1) #避免连续多次调用
+        client.enableApiControl(True, agent_name) #控制权交给api
+        client.armDisarm(True, agent_name) #解锁
 
         # set to start position
-        client.moveByVelocityAsync(0, 0, 0, 0.5, vehicle_name=agent_name)
+        client.moveByVelocityAsync(0, 0, 0, 0.5, vehicle_name=agent_name) #零速0.5s（airsim仿真秒），避免后续因为残留控制指令飘走
         yaw = math.atan2(waypoints[1][1] - waypoints[0][1], waypoints[1][0] - waypoints[0][0])
         start_pt = waypoints.pop(0)
         start_pt = [
             start_pt[0] + random.random() * 0.2 - 0.1,
             start_pt[1] + random.random() * 0.2 - 0.1,
             start_pt[2] + random.random() * 0.5 - 0.25,
-        ]
+        ] #浮动
         for _ in range(3):
             sleep(0.1)
             client.simSetVehiclePose(Pose(
                 Vector3r(*start_pt),
                 Quaternionr(0, 0, math.sin(yaw / 2), math.cos(yaw / 2))),
-                ignore_collision=True, vehicle_name=agent_name)
+                ignore_collision=True, vehicle_name=agent_name) #无视碰撞限制瞬移
         # align PID parameters to px4 default
         client.setAngleRateControllerGains(AngleRateControllerGains(
             roll_gains=PIDGains(0.2, 0.01, 0.001),
             pitch_gains=PIDGains(0.2, 0.01, 0.001),
             yaw_gains=PIDGains(0.2, 0.01, 0.001),
-        ))
+        ), agent_name) #为什么没设置agentname？（这里我加上）
         client.setAngleLevelControllerGains(AngleLevelControllerGains(
             roll_gains=PIDGains(2, 0, 0),
             pitch_gains=PIDGains(2, 0, 0),
@@ -187,9 +191,9 @@ def main():
         ), agent_name)
     sleep(0.5)
     for agent_name, waypoints in agents:
-        client.simGetCollisionInfo(agent_name)
+        client.simGetCollisionInfo(agent_name) #读取碰撞信息
 
-    p_target = torch.empty((B, 3))
+    p_target = torch.empty((B, 3)) #这里B是测试的无人机数量
     last_p = torch.empty((B, 3))
     forward_vec = torch.empty((B, 3))
     v = torch.empty((B, 3))
@@ -210,13 +214,13 @@ def main():
 
         q = torch.as_tensor([q.w_val, q.x_val, -q.y_val, -q.z_val])
         last_p[i] = torch.as_tensor([p.x_val, -p.y_val, -p.z_val])
-        forward_vec[i] = quaternion_to_matrix(q)[:, 0]
+        forward_vec[i] = quaternion_to_matrix(q)[:, 0] #q_e^b转换为R^e_b,第一列即无人机x轴在世界系表达
 
-    pbar = tqdm()
+    pbar = tqdm() #进度条对象
     hidden_state = None
     rate = Rate(15 * args.clockspeed)
     t_begin_real = time()
-    t_now = t_begin = state.timestamp / 1e9
+    t_now = t_begin = state.timestamp / 1e9 #仿真秒
     t_end = t_begin + 30
     # wind = [0, 0, 0]
     # a_set = [0, 0, 0]
@@ -233,10 +237,10 @@ def main():
         for agent in agent_names:
             responses = client.simGetImages([
                 airsim.ImageRequest("front_center_custom", airsim.ImageType.DepthPlanar, True)
-            ], agent)
-            depth.append(airsim.get_pfm_array(responses[0]))
-        depth = np.stack(depth)
-        depth_viz = np.uint8(np.clip(depth / 10 * 255, 0, 255))
+            ], agent) #True指pixels_as_float为True
+            depth.append(airsim.get_pfm_array(responses[0])) #get_pfm_array将一维浮点数据转换成二维numpy数组
+        depth = np.stack(depth) #[B,36,48]
+        depth_viz = np.uint8(np.clip(depth / 10 * 255, 0, 255)) #用于可视化
         depth_recorder.add_image(depth_viz.reshape(-1, 16*args.sr))
         # cv2.imshow('depth', np.uint8(np.clip(depth / 10 * 255, 0, 255)))
         # cv2.waitKey(1)
@@ -251,7 +255,7 @@ def main():
             p = torch.as_tensor([p.x_val, -p.y_val, -p.z_val])
             duration = t_now - t_begin
             if not done_flag[i]:
-                traveled_distance[i] += torch.norm(p - last_p[i]).item()
+                traveled_distance[i] += torch.norm(p - last_p[i]).item() #.item()将张量变成普通数值
                 traveled_time[i] = duration
             last_p[i] = p
 
@@ -262,15 +266,15 @@ def main():
 
             # step to the next checkpoint if distance < 1
             if not done_flag[i] and torch.norm(p_target[i] - p) < 1.5:
-                if waypoints:
+                if waypoints: #如果还有点就继续取下一个航点
                     x, y, z = waypoints.pop(0)
                     p_target[i] = torch.as_tensor([x, -y, -z])
-                else:
+                else: #如果没有点可取，说明已经到达目标位置
                     print(f"{agent_names[i]} arrived in {duration}s!")
                     done_flag[i] = True
-                    client.moveToPositionAsync(*target_pos[i], 0.5, vehicle_name=agent_names[i])
+                    client.moveToPositionAsync(*target_pos[i], 0.5, vehicle_name=agent_names[i]) #调用自带控制器飞向最终目标
                     if all(done_flag):
-                        t_end = t_now + 0.5
+                        t_end = t_now + 0.5 #如果提前都到了终点，直接加快结束
 
         # target velocity points to the target (with norm bounded by target_speed)
         target_v = p_target - last_p
@@ -280,22 +284,23 @@ def main():
         env_R = R.clone()
         fwd = R[:, :, 0].clone()
         up = torch.zeros_like(fwd)
-        fwd[:, 2] = 0
+        fwd[:, 2] = 0 #依旧投影到平面
         up[:, 2] = 1
         fwd = fwd / torch.norm(fwd, 2, -1, keepdim=True)
         R = torch.stack([fwd, torch.cross(up, fwd), up], -1)
 
         # state (in body frame): cat(velocity estimation, velocity target, rotation matrix, margin)
-        state = [torch.squeeze(target_v[:, None] @ R, 1), R[:, 2], extra]
+        # state = [torch.squeeze(target_v[:, None] @ R, 1), R[:, 2], extra] #注意矩阵不对
+        state = [torch.squeeze(target_v[:, None] @ R, 1), env_R[:, 2], extra]
         local_v = torch.squeeze(v[:, None] @ R, 1)
         if not args.no_odom:
             state.insert(0, local_v)
         state = torch.cat(state, -1)
 
         # normalize depth map
-        depth = torch.as_tensor(depth, device=device)[:, None]
+        depth = torch.as_tensor(depth, device=device)[:, None] #[B,1,36,48]
         x = 3 / depth.clamp_(0.3, 24) - 0.6
-        x = F.max_pool2d(x, (args.sr, args.sr))
+        x = F.max_pool2d(x, (args.sr, args.sr)) #[B,1,12,16]
 
         # obtain velocity setpoint and prediction from nnet
         # state = (state - states_mean) / states_std
@@ -303,15 +308,17 @@ def main():
         action, _, hidden_state = model(x, state, hidden_state)
         # action = action.cpu() * action_std + action_mean
         v_setpoint, v_est = (R @ action.cpu().reshape(B, 3, -1)).unbind(-1)
+        #R是cpu上创建的所以要把action先放到cpu
 
         # obtain acceleration setpoint
         a_setpoint = v_setpoint - v_est + ctl_error
         a_setpoint[:, 2] += 9.80665
 
+        #构造期望姿态
         # convert acceleration setpoint to rpy throttle
         throttle = torch.norm(a_setpoint, 2, -1)
         up_vec = a_setpoint / throttle[..., None]
-        throttle = throttle + local_v[:, 2] * local_v[:, 2].abs() * 0.01
+        throttle = throttle + local_v[:, 2] * local_v[:, 2].abs() * 0.01 #二次阻力补偿
 
         # forward vector is the normalized moving average of target vector
         forward_vec = env_R[..., 0] * 5 + p_target - last_p
@@ -319,6 +326,7 @@ def main():
         forward_vec /= torch.norm(forward_vec, 2, -1, True)
         left_vec = torch.cross(up_vec, forward_vec)
 
+        #转成欧拉角
         roll = torch.atan2(left_vec[:, 2], up_vec[:, 2])
         pitch = torch.asin(-forward_vec[:, 2])
         yaw = torch.atan2(forward_vec[:, 1], forward_vec[:, 0])
@@ -346,23 +354,23 @@ def main():
 
 if __name__ == '__main__':
     import shutil
-    shutil.copy(__file__, f"{log_dir}/eval.py")
+    shutil.copy(__file__, f"{log_dir}/eval.py") #保存实验使用的代码
     # os.environ['CUDA_VISIBLE_DEVICES'] = '1'
     ffmpeg_p = subprocess.Popen([
         '/usr/bin/ffmpeg',
-        '-f', 'x11grab',
+        '-f', 'x11grab', #从linux的x11桌面抓取画面
         '-video_size', '896x504',
-        '-i', ':0+512,340',
-        '-c:v', 'h264_nvenc',
-        '-vf', f'setpts={args.clockspeed}*PTS',
+        '-i', ':0+512,340', #左上角（512，340），和启动参数-WinX=512 -WinY=304配合
+        '-c:v', 'h264_nvenc', #设置视频编码器
+        '-vf', f'setpts={args.clockspeed}*PTS', #PTS是每一帧的显示时间戳，这里负责把因为仿真放缓乘以0.25的在视频中变回正常速度
         '-loglevel', 'error',
         '-an',
         f'{log_dir}/{datetime_str}.mp4'
-    ], stdin=subprocess.PIPE)
+    ], stdin=subprocess.PIPE) #airsim窗口录制
 
     def cleanup():
         with open(f"{log_dir}/traj_history.json", 'w') as f:
-            json.dump(traj_history, f)
+            json.dump(traj_history, f) #转换为json文本写入文件f
         ffmpeg_p.stdin.close()
         ffmpeg_p.wait()
         depth_recorder.close()
@@ -371,6 +379,6 @@ if __name__ == '__main__':
 
     try:
         main()
-        ffmpeg_p.send_signal(signal.SIGINT)
+        ffmpeg_p.send_signal(signal.SIGINT) #中断信号，请求正常结束
     finally:
         cleanup()
